3fbbdbd5cf1c7df42bf1b16441216145a24193bd
luiz <luizfernandoklein@live.com>
2020-07-20 18:14:26 -0300

    feat: Fetch github repositories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { InferGetServerSidePropsType } from 'next';
 
-const Main: React.FC = () => (
+interface Repo {
+  name: string;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('https://api.github.com/users/LuizFerK/repos');
+  const repos: Repo[] = await response.json();
+
+  const repoNames = repos.map(repo => repo.name);
+
+  return {
+    props: { repoNames }
+  }
+};
+
+const Main = ({ repoNames }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
   <>
     <Head>
       <title>Next - Main</title>
     </Head>
     <h1>Main</h1>
-    <p>
-      You are in the main page, to get more details, go to the {''}
-      <Link href="./detail">
-        <a>detail page</a>
-      </Link>
-    </p>
+    <h2>Repos:</h2>
+    {repoNames.map(repoName => (
+      <p>
+        <Link key={repoName} href="./detail">
+          <a>{repoName}</a>
+        </Link>
+      </p>
+    ))}
   </>
 );
 
